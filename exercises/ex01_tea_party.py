a0d9615bc8813fecf7f4cdb5c81565329bf27939
dhruvnpUNC <dhruvnp@ad.unc.edu>
2024-09-13 10:42:28 -0400

    updated files

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -10,7 +10,7 @@ def tea_bags(people: int) -> int:
 
 def treats(people: int) -> int:
     """Tells us how many treats we need to buy based on the amount of people."""
-    #Had issue with the function not returning properly until I realized in the return statement of the function I need to cast back the output from tea_bags * 1.5 as a int
+    # Had issue with the function not returning properly until I realized in the return statement of the function I need to cast back the output from tea_bags * 1.5 as a int
     return int(tea_bags(people=people) * 1.5)
 
 
@@ -21,10 +21,11 @@ def cost(tea_count: int, treat_count: int) -> float:
 
 def main_planner(guests: int) -> None:
     """Tells us how many treats and teabags to order, as well as the cost of the party."""
-    print("A Cozy Tea Party for " + str(guests) + " People!") # Didn't know that python doesn't allow you to concatenate integer variables with strings.
+    print("A Cozy Tea Party for " + str(guests) + " People!")  # Didn't know that python doesn't allow you to concatenate integer variables with strings.
     print("Tea Bags: " + str(tea_bags(people=guests)))
     print("Treats: " + str(treats(people=guests)))
     print("Cost: $" + str(cost(tea_count=tea_bags(people=guests), treat_count=treats(people=guests))))
+
 
 if __name__ == "__main__":
     main_planner(guests=int(input("How many guests are attending your tea party? ")))
