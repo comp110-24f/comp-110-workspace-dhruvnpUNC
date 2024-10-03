27ee0a742b3458ea8db6728d2c785b5985a010cf
dhruvnpUNC <dhruvnp@ad.unc.edu>
2024-10-03 16:37:01 -0400

    updated ex_03 worldle (with the right solution this time hopefully)

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -5,29 +5,27 @@ __author__: str = "730767591"
 
 def input_guess(secret_word: int) -> str:
     """Prompting the user to guess a word of a certain length"""
-    guessing: bool = True #conditional for the while loop
     guess: str = input(f"Enter a {secret_word} character word: ")
-    while guessing:
-        if len(guess) == secret_word:
-            guessing = False #when len of guess == secret_word guessing turns False exiting the while loop
-        else:
-            guess = input(f"That wasn't {secret_word} chars! Try again: ") #telling user to r   einput their guess
+    while len(guess) != secret_word:
+        guess = input(f"That wasn't {secret_word} chars! Try again: ") #telling user to r   einput their guess
     return guess
 
 
 def contains_char(secret_word: str, char_guess: str) -> bool:
+    """Checking to see if a character is in the secret word"""
     assert len(char_guess) == 1 #throws an error if char_guess is not equal to one
-    isTrue: bool = False # tracking to see if an character is in a given word
     idx: int = 0 # counter for while loop
     while idx < len(secret_word):
         if char_guess == secret_word[idx]:
-            isTrue = True #if a character is in the word, the variable turns true
+            return True #if a character is in the word, the variable turns true
         idx += 1
-    return isTrue
+    return False
 
 
 def emojified(guess: str, secret: str) -> str:
+    """Prints out the players guess as an emoji string representing the accuracy of their guess"""
     assert len(guess) == len(secret) #throws an error if guess and secret aren't the same length
+    #codes for the corresponding emoji boxes.
     WHITE_BOX: str = "\U00002B1C"
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
@@ -48,16 +46,18 @@ def emojified(guess: str, secret: str) -> str:
 def main(secret: str) -> None:
     max_turns: int = 6 #max turns a players is granted
     current_turn: int = 1 #current turn count
-    while current_turn <= max_turns:
+    while current_turn <= max_turns: #main while loop that checks to see if the player still has turns
         print(f"=== Turn {current_turn}/{max_turns} ===")
-        guess = input_guess(secret_word=len(secret)) #uses input_guess function to prompt player for input
-        print(emojified(guess=guess, secret=secret))
-        if secret == guess: #checks to see if the user has guessed the secret word exactly
-            print(f"You won in {current_turn}/{max_turns} turns!")
-            break #exits out of the while loop once user guesses the word. Gave me a lot of trouble with grading because I used exit() initially ;-;
-        current_turn += 1
+        guess = input_guess(secret_word=len(secret)) #prompts the user for a guess using our input_guess function
+        print(emojified(guess=guess, secret=secret)) #prints the users guess using our emojified function.
+        if guess == secret:
+            return print(f"You won in {current_turn}/{max_turns} turns!") #uses a return statement to exit the function.
+        current_turn += 1 # counter variable to prevent infinite loop.
 
-    print(f"x/{max_turns} - Sorry, try again tomorrow!")
+    if current_turn <= max_turns:
+        print(f"You won in {current_turn}/{max_turns} turns!")
+    else:
+        print(f"x/{max_turns} - Sorry, try again tomorrow!")
 
 
 if __name__ == "__main__":
---
+++ lessons/lists_practice.py
@@ -0,0 +1,3 @@
+# functions can take lists as an arguement
+# return or create lists
+# functions can modify lists
